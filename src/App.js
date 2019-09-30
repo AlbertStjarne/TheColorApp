3f5e5576e069cb78cd4669b30647730ba1533d1c
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-30 13:21:13 +0200

    adds palette footer with paletteName and emoji

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
   return (
     <div className='App'>
       <Palette palette={generatePalette(seedColors[4])} />
