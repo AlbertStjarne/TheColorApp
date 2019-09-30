1be18a367b475e6ca95fc38787caf07613d003f0
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-30 15:15:57 +0200

    fixing the palette route, finding the palette from the id in url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path='/' render={() => <h1>palette list goes here</h1>} />
-      <Route
-        exact
-        path='/palette/:id'
-        render={() => <h1>individual palette</h1>}
-      />
-    </Switch>
-    // <div className='App'>
-    //   <Palette palette={generatePalette(seedColors[4])} />
-    // </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' render={() => <h1>palette list goes here</h1>} />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div className='App'>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
