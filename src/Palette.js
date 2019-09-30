f82a31316dd859eee62630ae87be6d8408d4bd99
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-30 10:02:42 +0200

    adds google fonts, adds Navbar component, moving slider to Navbar, styling the Navbar

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import 'rc-slider/assets/index.css';
 import './Palette.css';
-import Slider, { Range } from 'rc-slider';
+import Navbar from './Navbar';
 
 class Palette extends Component {
   constructor(props) {
@@ -24,15 +23,7 @@ class Palette extends Component {
 
     return (
       <div className='Palette'>
-        <div className='slider'>
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className='Palette-colors'>{colorBoxes}</div>
       </div>
     );
