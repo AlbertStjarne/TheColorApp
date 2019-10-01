4a33dd5865f5db66b25416cec33c38273841d1fd
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-01 22:01:01 +0200

    adds the shades in SingleColorPalette component

--- src/App.js
+++ src/App.js
@@ -17,6 +17,17 @@ class App extends Component {
     return (
       <Switch>
         <Route
+          path='/palette/:paletteId/:colorId'
+          render={routeProps => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
+        />
+        <Route
           exact
           path='/'
           render={routeProps => (
@@ -33,10 +44,6 @@ class App extends Component {
               )}
             />
           )}
-        />
-        <Route
-          path='/palette/:paletteId/:colorId'
-          render={() => <SingleColorPalette />}
         />
       </Switch>
       // <div className='App'>
--- src/Palette.js
+++ src/Palette.js
@@ -30,6 +30,7 @@ class Palette extends Component {
         id={color.id}
         paletteId={id}
         moreUrl={`/palette/${id}/${color.id}`}
+        showLink={true}
       />
     ));
 
