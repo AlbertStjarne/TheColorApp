a2c6989fc80335b1de8df711c5d38d798fe1ab99
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-01 10:01:08 +0200

    styling MiniPalette (start of styling)

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,32 +2,44 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-  main: {
-    backgroundColor: 'purple',
-    border: '3px solid teal',
-  },
-  secondary: {
-    backgroundColor: 'pink',
-    '& h1': {
-      color: 'white',
-      '& span': {
-        backgroundColor: 'yellow',
-      },
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
     },
+  },
+  colors: {
+    backgroundColor: 'grey',
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative',
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem',
   },
 };
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   return (
-    <div className={classes.main}>
-      <section className={classes.secondary}>
-        Some extra text
-        <h1>
-          Mini Palette<span>Span inside h1</span>
-        </h1>
-        <span>Span outside h1</span>
-      </section>
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
