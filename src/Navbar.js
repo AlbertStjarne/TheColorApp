4738552d3df1910ebf222e3790043f3e5ec977d4
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-02 10:51:03 +0200

    adds Navbar to SingleColorPalette, fixing color format selector, hiding the Navbar slider, adds PaletteFooter component, adding it to SingleColorPalette and Palette

--- src/Navbar.js
+++ src/Navbar.js
@@ -27,7 +27,7 @@ class Navbar extends Component {
   }
 
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showingAllColors } = this.props;
     const { format } = this.state;
 
     return (
@@ -35,18 +35,20 @@ class Navbar extends Component {
         <div className='logo'>
           <Link to='/'>Color Picker App</Link>
         </div>
-        <div className='slider-container'>
-          <span>Level: {level}</span>
-          <div className='slider'>
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-            />
+        {showingAllColors && (
+          <div className='slider-container'>
+            <span>Level: {level}</span>
+            <div className='slider'>
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+              />
+            </div>
           </div>
-        </div>
+        )}
         <div className='select-container'>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value='hex'>HEX - #FFFFFF</MenuItem>
