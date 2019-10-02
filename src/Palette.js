ac0e3102b3b75f5bc1448c9f0ea8dfa9ca17cfe3
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-02 15:49:46 +0200

    refactoring ColorBox styling using withStyles

--- src/Palette.js
+++ src/Palette.js
@@ -31,7 +31,7 @@ class Palette extends Component {
         id={color.id}
         paletteId={id}
         moreUrl={`/palette/${id}/${color.id}`}
-        showLink={true}
+        showingFullPalette={true}
       />
     ));
 
