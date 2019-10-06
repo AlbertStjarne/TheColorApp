ab2d798b75d370e7b323b6e71c9d685289695d9f
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-06 15:12:36 +0200

    adds delete function to the delete button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -146,6 +146,12 @@ class NewPaletteForm extends Component {
     this.props.history.push('/');
   }
 
+  removeColor(colorName) {
+    this.setState({
+      colors: this.state.colors.filter(color => color.name !== colorName),
+    });
+  }
+
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -245,8 +251,10 @@ class NewPaletteForm extends Component {
           <div className={classes.drawerHeader} />
           {this.state.colors.map(color => (
             <DraggableColorBox
+              key={color.name}
               color={color.color}
               name={color.name}
+              handleClick={() => this.removeColor(color.name)}
             ></DraggableColorBox>
           ))}
         </main>
