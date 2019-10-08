7e128bbc3334ca4d75a5e14f15f5199687d2b316
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-08 13:44:22 +0200

    adds ability to click away the dialog, installing emoji-mart, adds emoji-mart to dialog

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -7,6 +7,8 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 class PaletteMetaForm extends Component {
   constructor(props) {
@@ -40,20 +42,21 @@ class PaletteMetaForm extends Component {
 
   render() {
     const { newPaletteName } = this.state;
+    const { hideForm, handleSubmit } = this.props;
 
     return (
       <Dialog
         open={this.state.open}
-        onClose={this.handleClose}
         aria-labelledby='form-dialog-title'
+        onClose={hideForm}
       >
         <DialogTitle id='form-dialog-title'>Choose A Palette Name</DialogTitle>
-        <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
+        <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
           <DialogContent>
             <DialogContentText>
               Please enter a name for your new palette. Make sure it's unique.
             </DialogContentText>
-
+            <Picker />
             <TextValidator
               label='Palette Name'
               value={newPaletteName}
@@ -66,7 +69,7 @@ class PaletteMetaForm extends Component {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.handleClose} color='primary'>
+            <Button onClick={hideForm} color='primary'>
               Cancel
             </Button>
             <Button variant='contained' color='primary' type='submit'>
