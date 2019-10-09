a84059e99074b47e9e272a3e5aa14e28615574d5
Albert Stjärne <albert.stjarne@gmail.com>
2019-10-09 16:58:57 +0200

    adds functionality to delete palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -10,7 +10,7 @@ class PaletteList extends Component {
   }
 
   render() {
-    const { palettes, classes } = this.props;
+    const { palettes, classes, deletePalette } = this.props;
 
     return (
       <div className={classes.root}>
@@ -24,6 +24,9 @@ class PaletteList extends Component {
               <MiniPalette
                 {...palette}
                 handleClick={() => this.goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
               />
             ))}
           </div>
